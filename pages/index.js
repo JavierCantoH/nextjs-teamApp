d2f122bc2483ee608f10dc0fd0fd44cb84c7d7a2
Luis Javier Canto Hurtado <javier.canto.h@gmail.com>
2021-10-10 14:04:59 -0500

    Layout component

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,16 @@
 import Head from 'next/head'
-import Footer from '../components/Footer'
-import Navbar from '../components/Navbar'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>Homepage</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
       <Link href="/players/">
         <a>See Player Listing</a>
       </Link>
-      <Footer />
     </div>
   )
 }
