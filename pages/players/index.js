ba7f1407f69b9ccb78dc80a0427bc3bda93e3361
Luis Javier Canto Hurtado <javier.canto.h@gmail.com>
2021-10-10 14:46:46 -0500

    Fetching data from an external API json placeholder

--- pages/players/index.js
+++ pages/players/index.js
@@ -1,9 +1,28 @@
-const Players = () => {
-    return (
-      <div>
-        <h1>All Player</h1>
-      </div>
-    );
+import styles from '../../styles/Players.module.css'
+
+// using API json place holder to import players data
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
+  return {
+    props: { players: data }
+  }
+}
+
+const Players = ({ players }) => {
+  return (
+    <div>
+      <h1>All Players</h1>
+        {players.map(player => (
+          <div key={player.id}>
+            <a className={styles.single}>
+              <h3>{ player.name }</h3>
+            </a>
+          </div>
+        ))}
+    </div>
+  );
 }
    
 export default Players;
